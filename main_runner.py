134bfd69a23cb14b367bf92e0855e8d21a24dad7
Mihir Kale <mihir.p.kale@gmail.com>
2018-10-07 11:35:06 -0400

    Update main_runner.py
    Added function skeletons

--- main_runner.py
+++ main_runner.py
@@ -1,1 +1,14 @@
 ##Created by Mihir Kale, Oct 7, 2018
+
+#Alerts will be sent when weight goes below this
+THRESHHOLD_WEIGHT = 800
+
+#User will be alerted at this intervals (in hours)
+ALERT_FREQ_HRS = 6
+
+
+def get_latest_reads():
+	#code here
+	
+def alert_user():
+	#code here

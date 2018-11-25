827dd5b6226ca4cecc4ba0cbddf548376915888e
mihir kale <mihir.p.kale@gmail.com>
2018-11-25 01:11:06 +0000

    Still to write ZERO reading time limit logic to reset, but otherwise it all works

--- smart_shelf.py
+++ smart_shelf.py
@@ -10,17 +10,43 @@ def cleanAndExit():
     print "Bye!"
     sys.exit()
 
+
+#these values are standard, dont mess with them
 hx = HX711(5, 6)
 hx.set_reading_format("LSB", "MSB")
-#hx.set_reference_unit(-411)
+
+
+#Set this for calibration of the load cell
+hx.set_reference_unit(-355)
+
+#ignore these, let them be where they are
 hx.reset()
 hx.tare()
 
-THRESHHOLD_TRIGGER = 3000
+
+#########
+## These are important configuration parameters
+########
+
+#Weight below which a FINISHED alert will be generated
+THRESHHOLD_TRIGGER = 1200
+
+
+# This is to bypass the error in the load cell
+# sometimes the load cell gives random values at zero load
+# adjust this value is required
+ZERO_RANGE = 250
+
+# keep these as they are
 LAST_HIGH_READING = 0
-LAST_RELOAD_TIME = 0
-LAST_ALERT_SENT = 0
-ZERO_RANGE = 200
+LAST_RELOAD_TIME = datetime.datetime.now() - datetime.timedelta(hours=5)
+LAST_ALERT_SENT = datetime.datetime.now() - datetime.timedelta(hours=5)
+
+# These values are in Minutes, keep them small for demo
+# In real life they would be in hours
+# to convert to hours, change the paramter where-ever "timedelta" is called
+ALERT_FREQ = 2
+SHELF_LIFE = 1
 
 
 
@@ -35,17 +61,24 @@ def call_alert(message):
 
 while True:
 	try:
-       		val = hx.get_weight(5)
-        	print val
+       		val = max(0,int(hx.get_weight(5)))
+        	print "Current reading is...:" +str(val)
 		if val > ZERO_RANGE:
 			if val < THRESHHOLD_TRIGGER:
-				call_alert("FINISHED")
+				if datetime.datetime.now() > (LAST_ALERT_SENT + datetime.timedelta(minutes=ALERT_FREQ)):
+					LAST_ALERT_SENT = datetime.datetime.now()	
+					call_alert("FINISHED")
+				else: print "Last FINISHED alert was sent and have not crossed ALERT FREQ, so will not send"
 			elif val > LAST_HIGH_READING:
 				LAST_HIGH_READING = val
 				LAST_RELOAD_TIME = datetime.datetime.now()
-				print "NEW RELOAD READ AND RELOAD TIME IS....", +str(LAST_HIGH_READING)+"....."+str(LAST_RELOAD_TIME)
-			elif datetime.datetime.now() < (LAST_RELOAD_TIME + datetime.timedelta(days=SHELF_LIFE)):
-				call_alert("EXPIRED")
+				print "NEW RELOAD READ AND RELOAD TIME IS...." +str(LAST_HIGH_READING)+"....."+str(LAST_RELOAD_TIME)
+			elif datetime.datetime.now() > (LAST_RELOAD_TIME + datetime.timedelta(minutes=SHELF_LIFE)):
+				if datetime.datetime.now() > (LAST_ALERT_SENT + datetime.timedelta(minutes=ALERT_FREQ)):
+					LAST_ALERT_SENT = datetime.datetime.now()
+					call_alert("EXPIRED")
+				else: print "LAST EXPIRED alert was sent and have not yet crossed ALERT FREQ, so will not send new Alert"	
+			else: print "No action required, current reading is higher than THRESHHOLD"
 		else: print "val is less than ZERO_RANGE, will do nothing"
 
 			

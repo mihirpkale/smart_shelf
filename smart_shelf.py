55801551e5f63cf71cb9b18df4a64a589f917485
parag kale <parag.kale@siemens.com>
2019-01-30 21:40:31 -0500

    indentation

--- smart_shelf.py
+++ smart_shelf.py
@@ -67,37 +67,36 @@ def call_alert(message):
 
 while True:
 	try:
-       	val = max(0,int(hx.get_weight(5)))
-        print "Current reading is...:" +str(val)
-		if val==0:
-			if datetime.datetime.now() > (LAST_RELOAD_TIME + datetime.timedelta(seconds=RESET_INTERVAL)):
-				LAST_RELOAD_TIME = datetime.time(0,0,0)
-				print "Resetting LAST_RELOAD_TIME, Shelf is empty for a considerable amount of time"
-		if val > ZERO_RANGE:
-			if val < THRESHHOLD_TRIGGER:
-				if datetime.datetime.now() > (LAST_ALERT_SENT + datetime.timedelta(minutes=ALERT_FREQ)):
-					LAST_ALERT_SENT = datetime.datetime.now()
-					call_alert("FINISHED")
-				else: print "Last alert was recently sent. Next alert will be sent after ALERT FREQ interval has passed."
-			elif datetime.datetime.now() > (LAST_RELOAD_TIME + datetime.timedelta(minutes=SHELF_LIFE)):
-				if datetime.datetime.now() > (LAST_ALERT_SENT + datetime.timedelta(minutes=ALERT_FREQ)):
-					LAST_ALERT_SENT = datetime.datetime.now()
-					call_alert("EXPIRED")
-				else: print "Last alert was recently sent. Next alert will be sent after ALERT FREQ interval has passed."
-			elif val >= (LAST_HIGH_READING + 100):
-				#Keeping the 100 gms to account for sensor reading variations
-				if LAST_RELOAD_TIME > (datetime.datetime.now() - datetime.timedelta(minutes=5)):
-					#which means we dont want to reset the LAST RELOAD TIME, it was set less than 5 minutes ago
-					print "New load sensed but last reload was less than 5 minutes ago so not re-setting last reload time"
-				else:
-					LAST_HIGH_READING = val
-					LAST_RELOAD_TIME = datetime.datetime.now()
-					print "NEW LOAD DETECTED at " +str(LAST_HIGH_READING)+".....at time..."+str(LAST_RELOAD_TIME)
-				
-			else: print "No action required, current reading is higher than THRESHHOLD"
-		else: print "Recorded weight is less than ZERO_RANGE, Shelf appears to be empty. No action required"
+			val = max(0,int(hx.get_weight(5)))
+			print "Current reading is...:" +str(val)
+			if val==0:
+				if datetime.datetime.now() > (LAST_RELOAD_TIME + datetime.timedelta(seconds=RESET_INTERVAL)):
+					LAST_RELOAD_TIME = datetime.time(0,0,0)
+					print "Resetting LAST_RELOAD_TIME, Shelf is empty for a considerable amount of time"
+			if val > ZERO_RANGE:
+				if val < THRESHHOLD_TRIGGER:
+					if datetime.datetime.now() > (LAST_ALERT_SENT + datetime.timedelta(minutes=ALERT_FREQ)):
+						LAST_ALERT_SENT = datetime.datetime.now()
+						call_alert("FINISHED")
+					else: print "Last alert was recently sent. Next alert will be sent after ALERT FREQ interval has passed."
+				elif datetime.datetime.now() > (LAST_RELOAD_TIME + datetime.timedelta(minutes=SHELF_LIFE)):
+					if datetime.datetime.now() > (LAST_ALERT_SENT + datetime.timedelta(minutes=ALERT_FREQ)):
+						LAST_ALERT_SENT = datetime.datetime.now()
+						call_alert("EXPIRED")
+					else: print "Last alert was recently sent. Next alert will be sent after ALERT FREQ interval has passed."
+				elif val >= (LAST_HIGH_READING + 100):
+					#Keeping the 100 gms to account for sensor reading variations
+					if LAST_RELOAD_TIME > (datetime.datetime.now() - datetime.timedelta(minutes=5)):
+						#which means we dont want to reset the LAST RELOAD TIME, it was set less than 5 minutes ago
+						print "New load sensed but last reload was less than 5 minutes ago so not re-setting last reload time"
+					else:
+						LAST_HIGH_READING = val
+						LAST_RELOAD_TIME = datetime.datetime.now()
+						print "NEW LOAD DETECTED at " +str(LAST_HIGH_READING)+".....at time..."+str(LAST_RELOAD_TIME)
+				else: print "No action required, current reading is higher than THRESHHOLD"
+			else: print "Recorded weight is less than ZERO_RANGE, Shelf appears to be empty. No action required"
 
         	hx.power_down()
         	hx.power_up()
-    	except (KeyboardInterrupt, SystemExit):
+    except (KeyboardInterrupt, SystemExit):
         	cleanAndExit()
